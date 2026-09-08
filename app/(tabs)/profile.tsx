dab5bceeb800b0938fd76c66736dad7169769a31
arunpravin125 <162274540+arunpravin125@users.noreply.github.com>
2026-09-08 17:30:36 +0530

    profilescreen created

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,100 @@
-import { Text, View } from 'react-native'
+import { ActivityIndicator, Image, RefreshControl, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { useCurrentUser } from '../../hooks/useCurrentUser'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import SignOutButton from '../../components/SignOutButton'
+import { Feather } from '@expo/vector-icons'
+import { format } from 'date-fns'
+import { usePosts } from '../../hooks/usePosts'
+import PostsList from '../../components/PostsList'
+import { useState } from 'react'
 
 export default function ProfileScreen() {
+    const { currentUser, isLoading } = useCurrentUser()
+    const inserts = useSafeAreaInsets()
+    //  const [isRefetching,setIsRefetching] = useState(false)
+    const { posts: userPosts, refetch: RefetchPosts, isLoading: isRefetching } = usePosts(currentUser?.username)
+
+
+    if (isRefetching) {
+        return (
+            <View className='flex-1 bg-white items-center justify-center'>
+                <ActivityIndicator size="large" color={"#1DA1f2"} />
+            </View>
+        )
+    }
+    const handlePullToRefresh = async () => {
+
+        await RefetchPosts()
+
+    }
+
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text>ProfileScreen</Text>
-        </View>
+        <ScrollView refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={handlePullToRefresh} tintColor={"#1da1f2"} />}>
+
+            <SafeAreaView className="flex-1 bg-white">
+                {/* header */}
+                <View className='flex-row items-center justify-between px-4 py-3 border-b  border-gray-100' >
+                    <View>
+                        <Text className='text-xl font-bold text-gray-900'>{currentUser.firstName} {currentUser.lastName}</Text>
+                        <Text className='text-gray-500 text-sm'>{userPosts?.length} Posts</Text>
+                    </View>
+                    <SignOutButton />
+                </View>
+
+                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 + inserts.bottom }} className='flex-1'>
+
+                    <View className='relative' >
+                        <Image resizeMode='cover' className='w-full h-48' source={{ uri: currentUser.bannerImage || "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&h=400&fit=crop" }} />
+
+                    </View>
+                    <View className='px-4 pb-4 border-b border-gray-100'>
+                        <View className='flex-row justify-between items-end -mt-16 mb-4' >
+                            <Image source={{ uri: currentUser.profilePicture }} className='w-32 h-32 rounded-full border-4 border-white' />
+
+                            <TouchableOpacity className='border border-gray-300 px-6 py-2 rounded-full'>
+                                <Text className='font-semibold text-gray-900'>Edit profile</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View className='mb-4' >
+                            <View className='flex-row items-center mb-1' >
+                                <Text className='text-xl font-bold text-gray-900 mr-1'>{currentUser.firstName} {currentUser.lastName}</Text>
+                                <Feather name="check-circle" size={20} color={"#1da1f2"} />
+
+                            </View>
+                            <Text className='text-gray-500 mb-2'>@{currentUser.username}</Text>
+                            <Text className='text-gray-900 mb-3'>{currentUser.bio}</Text>
+                            <View className='flex-row items-center mb-2'>
+                                <Feather name='map-pin' size={16} color='#657786' />
+                                <Text className='text-gray-500 ml-2' >{currentUser.location}</Text>
+                            </View>
+                            <View className='flex-row items-center mb-3'>
+                                <Feather name="calendar" size={16} color="#657786" />
+                                <Text className='text-gray-500 ml-2 text-sm'>Joined {format(new Date(currentUser.createdAt), 'MMMM yyyy')} </Text>
+                            </View>
+
+                            <View className='flex-row'>
+                                <TouchableOpacity className='mr-6'>
+                                    <Text className='text-gray-900'>
+                                        <Text className='font-bold'>{currentUser.following?.length}</Text>
+                                        <Text className='text-gray-500'> Following</Text>
+                                    </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity >
+                                    <Text className='text-gray-900'>
+                                        <Text className='font-bold'>{currentUser.followers?.length}</Text>
+                                        <Text className='text-gray-500'> Followers</Text>
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                    </View>
+                    <PostsList username={currentUser?.username} />
+
+                </ScrollView>
+
+            </SafeAreaView>
+        </ScrollView>
     )
 }
